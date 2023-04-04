6e893bcb4a0ef478f8e6c71ab7cdd57fc7a20d3e
BuBuSS <bubus258@gmail.com>
2023-04-04 15:51:26 +0200

    format lambda directory

--- lambda/function_pointers.cpp
+++ lambda/function_pointers.cpp
@@ -1,23 +1,27 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 using namespace std;
 
 /// Computes n-th root of x
-double sqrtn(int n, double x){
-	return exp( log(x) / n); 
+double sqrtn(int n, double x)
+{
+    return exp(log(x) / n);
 }
-double power(double  x, double y){
-	return exp( log(x) * y);
+double power(double x, double y)
+{
+    return exp(log(x) * y);
 }
-template <typename F, typename G>
-double function(int n, double y, double z, F f, G g){
-	return (f(n,y) > z) ? g(z, y) : g(y, z);
+template <typename F, typename G> double function(int n, double y, double z, F f, G g)
+{
+    return (f(n, y) > z) ? g(z, y) : g(y, z);
 }
-int main(){
-	double (*fp1)(int, double) = sqrtn;
+int main()
+{
+    double (*fp1)(int, double) = sqrtn;
     double (*fp2)(double, double) = power;
-	double (*fp3)(int, double, double, double (*)(int, double), double (*)(double, double)) = function<double (*)(int, double), double (*)(double, double)>;
-	cout << fp3(2, 10, 3, fp1, fp2) << endl;
-	cout << fp3(3, 10, 3, fp1, fp2) << endl;
-	return 0;
+    double (*fp3)(int, double, double, double (*)(int, double), double (*)(double, double)) =
+        function<double (*)(int, double), double (*)(double, double)>;
+    cout << fp3(2, 10, 3, fp1, fp2) << endl;
+    cout << fp3(3, 10, 3, fp1, fp2) << endl;
+    return 0;
 }
--- lambda/printer.cpp
+++ lambda/printer.cpp
@@ -1,41 +1,44 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <vector>
-#include <fstream>
-#include <algorithm>
 
 using namespace std;
-class Printer{
+class Printer
+{
   private:
-    std::ostream& out;
+    std::ostream &out;
     std::string prefix;
     std::string suffix;
 
   public:
-    Printer(std::ostream& out, std::string prefix, std::string suffix) : out(out), prefix(prefix), suffix(suffix)
-    {}
-
-    void operator() (auto element)
+    Printer(std::ostream &out, std::string prefix, std::string suffix) : out(out), prefix(prefix), suffix(suffix)
     {
-      this->out << this->prefix << element << this->suffix; 
+    }
+
+    void operator()(auto element)
+    {
+        this->out << this->prefix << element << this->suffix;
     }
 };
 
-int main(){
-  /// Creates unary functor that takes one argument x (of any type)
-  /// and outputs to given stream x surrounded by given prefix na postfix
-  /// e.g. in the following  [ x ]
-  /// Hint: define function template.
-  Printer printer(std::cout,  "[ ", " ] " );
-  printer("hello");    // [ hello ]
-  std::cout << "\nv = ";
-  std::vector<int> v = {1, 2, 3, 4};
-  std::for_each(v.begin(), v.end(), printer);  // v = [ 1 ] [ 2 ] [ 3 ] [ 4 ]
+int main()
+{
+    /// Creates unary functor that takes one argument x (of any type)
+    /// and outputs to given stream x surrounded by given prefix na postfix
+    /// e.g. in the following  [ x ]
+    /// Hint: define function template.
+    Printer printer(std::cout, "[ ", " ] ");
+    printer("hello"); // [ hello ]
+    std::cout << "\nv = ";
+    std::vector<int> v = {1, 2, 3, 4};
+    std::for_each(v.begin(), v.end(), printer); // v = [ 1 ] [ 2 ] [ 3 ] [ 4 ]
 
-  std::ofstream file("myFile.txt");
-  Printer filePrinter(file, "- ", "\n");
-  filePrinter(5);
-  filePrinter("My text");	
-	return 0;
+    std::ofstream file("myFile.txt");
+    Printer filePrinter(file, "- ", "\n");
+    filePrinter(5);
+    filePrinter("My text");
+    return 0;
 }
 /** myFile.txt
 - 5
